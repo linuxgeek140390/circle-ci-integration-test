6366f0f18a280a3b3e60e609bfa65628fbe22381
Piyush <sharmapiyush500@gmail.com>
2023-09-12 16:09:05 +0530

    ec2 code added

--- ec2.sh
+++ ec2.sh
@@ -9,7 +9,7 @@ sudo ./aws/install
 echo "We are on branch $CIRCLE_BRANCH"
 env="$CIRCLE_BRANCH"
 echo "This execution will create ec2 instance for $env environment"
-aws ec2 run-instances --image-id ami-0b2b4f610e654d9ac --count 1 --instance-type t2.micro --key-name circleci --security-group-ids sg-0fe321f40b7431450 --subnet-id subnet-09e865d82bb0c018d --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=${env}}]' --region us-west-2
+aws ec2 run-instances --image-id ami-0b2b4f610e654d9ac --count 1 --instance-type t2.micro --key-name circleci --security-group-ids sg-0fe321f40b7431450 --subnet-id subnet-09e865d82bb0c018d --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${env}}]" --region us-west-2
 
 
 
